1184f8780b10a9e9f073bbc5ec241f3271c44985
KevinMuniz1 <munizkevin@outlook.com>
2023-11-06 14:33:47 -0500

    test code

--- little lemon capstone/Menu.swift
+++ little lemon capstone/Menu.swift
@@ -6,8 +6,47 @@
 //
 
 import SwiftUI
+import Foundation
+import CoreData
+
 
 struct Menu: View {
+    
+    @Environment(\.managedObjectContext) private var viewContext
+    
+    func getMenuData() {
+        PersistenceController.shared.clear()
+        
+        let menuUrl = URL(string: "https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu.json")
+        
+        guard let menuUrl = menuUrl else {
+            print("This is not valid")
+            return
+        }
+        let request = URLRequest(url: menuUrl)
+        
+        let task = URLSession.shared.dataTask(with: request){data,response, error in
+            if let data = data {
+                let fullMenu = try? JSONDecoder().decode(MenuList.self, from: data)
+                for items in fullMenu!.menu {
+                    let newDish = Dish(context: viewContext)
+                    newDish.title = items.title
+                    newDish.price = items.price
+                    newDish.image = items.image
+                    print(items.title)
+                    print(items.price)
+                    print(items.image)
+                }
+                try? viewContext.save()
+            } else {
+                print("Invalid")
+                return
+            }
+        }
+        
+        task.resume()
+    }
+    
     var body: some View {
         VStack {
             Text("Little Lemon")
@@ -16,12 +55,27 @@ struct Menu: View {
             Text("Chicago")
                 .font(.title2)
             Text("Description")
-            List {
-                Text("This is a test 2")
+            FetchedObjects(){(dishes: [Dish]) in
+                List {
+                    ForEach(dishes, id: \.id){dish in
+                        HStack(alignment: .center, spacing: 50) {
+                            Text(dish.title!)
+                            Text(dish.price!)
+                            AsyncImage(url: URL(string: dish.image!)){image in
+                                image.image?.resizable()
+                            }.frame(width: 50, height: 50, alignment: .trailing)
+                        }
+                    }
                 }
             }
-        }
+        
+
+        }.onAppear(perform: {
+            PersistenceController.shared.clear()
+            getMenuData()
+        })
     }
+}
 
 
 #Preview {
